914d61295e0e626ac44532ea00f93eb3dc83c2c0
Irene G <33572837+irenegrone@users.noreply.github.com>
2021-06-09 13:30:26 +0100

    Update Eruptions_Volcanoes_analysis.R

--- Eruptions_Volcanoes_analysis.R
+++ Eruptions_Volcanoes_analysis.R
@@ -8,15 +8,26 @@
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(flexdashboard)
 
 
 
 # Load data ####
+
 Eruption_FILE_PATH = paste0(getwd() ,"/data/GVP_Eruption_Results.xlsx")
 Holocene_FILE_PATH = paste0(getwd() ,"/data/GVP_Volcano_List_Holocene.xlsx")
 Pleistocene_FILE_PATH = paste0(getwd() ,"/data/GVP_Volcano_List_Pleistocene.xlsx")
 
+# Eruptions
 eruption_data <- read_xlsx(Eruption_FILE_PATH, sheet = "Eruption List", skip = 1)
-
 eruption_events_data <- read_xlsx(Eruption_FILE_PATH, sheet = "Events", skip = 1)
+
+# Volcano list Holocene
+holocene_volcanoes <- read_xlsx(Holocene_FILE_PATH, sheet = "Holocene Volcano List", skip = 1)
+
+# Volcano list Pleistocene
+pleistocene_volcanoes <- read_xlsx(Pleistocene_FILE_PATH, sheet = "Pleistocene Volcano List", skip = 1)
+
+
+# Data quality and cleaning ####
 
