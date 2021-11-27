703103bd283fbea5ad7a22e5413f20909ddc2186
Irene G <33572837+irenegrone@users.noreply.github.com>
2021-11-27 18:27:08 +0000

    Updates

--- Eruptions_Volcanoes.R
+++ Eruptions_Volcanoes.R
@@ -2,14 +2,16 @@
 
 # title: "Eruptions  and Volcanoes"
 # author: "Irene Grone"
-# date: "09/06/2021"
+# date:  "December 2021"
 
 
 # Import libraries ####
 
-library(tidyverse)
+library(dplyr)
 library(readxl)
+library(magrittr)
 library(lubridate)
+library(ggplot2)
 library(flexdashboard)
 
 
@@ -23,22 +25,42 @@ Pleistocene_FILE_PATH = paste0(getwd() ,"/data/GVP_Volcano_List_Pleistocene.xlsx
 
 # Eruptions
 eruption_data <- read_xlsx(Eruption_FILE_PATH,
-                           sheet = "Eruption List", skip = 1)
+                           sheet = "Eruption List",
+                           skip = 1)[, c("Volcano Number",
+                                         "Volcano Name",
+                                         "Eruption Number",
+                                         "Area of Activity",
+                                         "VEI",
+                                         "Start Year",
+                                         "Start Month",
+                                         "Start Day",
+                                         "Evidence Method (dating)",
+                                         "Latitude",
+                                         "Longitude")]
+
 eruption_events_data <- read_xlsx(Eruption_FILE_PATH,
-                                  sheet = "Events", skip = 1)
+                                  sheet = "Events",
+                                  skip = 1)[, c("Volcano Number",
+                                                "Volcano Name",
+                                                "Eruption Number",
+                                                "Eruption Start Year",
+                                                "Event Number",
+                                                "Event Type")]
+
 
 # Volcano list Holocene
 holocene_volcanoes <- read_xlsx(Holocene_FILE_PATH,
-                                sheet = "Holocene Volcano List", skip = 1)
+                                sheet = "Holocene Volcano List",
+                                skip = 1)
+
 
 # Volcano list Pleistocene
 pleistocene_volcanoes <- read_xlsx(Pleistocene_FILE_PATH,
-                                   sheet = "Pleistocene Volcano List", skip = 1)
+                                   sheet = "Pleistocene Volcano List",
+                                   skip = 1)
 
 
-# EDA ####
-
-# check NAs
+# DATA CLEANING ####
 
 sapply(eruption_data, function(x) sum(is.na(x)))
 
@@ -46,4 +68,12 @@ sapply(eruption_events_data, function(x) sum(is.na(x)))
 
 sapply(holocene_volcanoes, function(x) sum(is.na(x)))
 
+
+# Pleistocene
 sapply(pleistocene_volcanoes, function(x) sum(is.na(x)))
+
+pleistocene_volcanoes <- pleistocene_volcanoes %>%
+  select(-c('Activity Evidence',
+            'Last Known Eruption',
+            'Dominant Rock Type'))
+
